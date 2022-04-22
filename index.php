462890673684a05917c2f66cedf1da2c6da51208
Plitochnik <88889884+Plitochnik@users.noreply.github.com>
2022-04-22 19:56:20 +0300

    index.php
    It is view of whole program.

---
+++ index.php
@@ -0,0 +1,25 @@
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Gen php</title>
+</head>
+<body>
+<h1 class="fs-5">
+    Matrix generation
+</h1>
+<div class="container mt-2">
+    <form action="for_checker.php" method="post">
+        <input type="text" name="line" placeholder="Amout of lines" class="form-control"><br>
+        <input type="text" name="column" placeholder="Amout of columns" class="form-control form-control-sm"><br>
+        <button type="submit" class="btn btn-warning">Готово</button>
+    </form>
+</div>
+</body>
+</html>
